d5f8e6edabcbf03f1d47e31878bfd9ca1eef938b
hfmontalvan <hfmontalvan@utpl.edu.ec>
2026-01-11 22:49:17 -0500

    el botón modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/burns.gif";
     console.log("ingresa a saludar");
 }
