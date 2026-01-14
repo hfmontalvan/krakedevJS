53a24f603dfc5e5b22f4d4f40f2e42299e0033ff
hfmontalvan <hfmontalvan@utpl.edu.ec>
2026-01-13 22:55:13 -0500

    funciones recuperarInt y recuperarFloat

--- ejercicioGit/utilitarios/saludo.js
+++ ejercicioGit/utilitarios/saludo.js
@@ -1,14 +1,30 @@
-saludar=function(){
+saludar = function(){
     //recuperar el valor de la caja de texto txtNombre
-    let nombre=recuperarTexto("txtNombre");
+    let nombre = recuperarTexto("txtNombre");
     //recuperar el valor de la caja de texto txtApellido
-    let apellido=recuperarTexto("txtApellido");
+    let apellido = recuperarTexto("txtApellido");
+     //recuperar el valor de la caja de texto txtEdad
+    let edad = recuperarInt("txtEdad");
+    //recuperar el valor de la caja de texto txtEstatura
+    let estatura = recuperarFloat("txtEstatura");
 }
 
-recuperarTexto=function(idComponente){
+recuperarTexto = function(idComponente){
     let componente;
     let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
     return valorIngresado;
+}
+
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
 }
