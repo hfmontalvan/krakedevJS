9eda8a5b70c326a413ab773e50840204dd6cda41
hfmontalvan <hfmontalvan@utpl.edu.ec>
2026-02-15 15:59:31 -0500

    Reto 37 ejercicios Bucles - For

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,14 @@
+mostrarNumeros = function(){
+    console.log("Antes del For");
+    for(let i=0;i<4;i++){
+        console.log(i);
+    }
+    console.log("Despues de For");
+}
+impares=function(){
+    console.log("Antes del For");
+    for(let i=1;i<=21;i++){
+        console.log(i);
+    }
+    console.log("Despues del For")
+}
